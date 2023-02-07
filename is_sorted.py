47a33cb97c0cd8a7616379d81d8c63dabcf880ab
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-02-07 13:53:15 -0800

    Added docstring to is_sorted function

---
+++ is_sorted.py
@@ -0,0 +1,12 @@
+def is_sorted(number_list):
+    """
+    Check if number_list is sorted.
+
+    This function returns True if number_list is sorted in non-decreasing order, otherwise False. Non-decreasing order
+    means that each integer in the list is equal to or greater than the preceding integer.
+
+    :param number_list: a list of integer numbers
+    :precondition: number_list must be a list of integer numbers
+    :postcondition: correctly asserts True that number_list is sorted, else False
+    :return: True if number_list is sorted, else False, as a boolean
+    """
